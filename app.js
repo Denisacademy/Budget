4dff67289d9992600672f079abf4adc85f222441
Denisacademy <brokenhandq3@yandex.ru>
2020-07-31 23:14:50 +0300

    constructor exp and inc

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
 var budgetController = (function() {
   
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],  //new item
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
+
+  return {
+    addItem : function(type, des, val) {
+      var newItem, ID;
+
+      //create new ID
+      if(data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length-1].id + 1;
+
+      } else {
+        ID = 0;
+      } 
+
+      //Create new item   based on type inc or exp
+      if(type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if(type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+      
+      //push it into our data structure
+      data.allItems[type].push(newItem);
+      
+      //return the new elem
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
+
 })();
 
 //UI controller====***
 var UIcontroller = (function() {
   // Some code -- collect neccesary fields
 
-  var DOMstring = {
+  var DOMstring = {  //short way
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
@@ -46,12 +99,12 @@ var controller = (function(budgetCtrl, UICtrl) {
  
   
   var ctrlAddItem = function() {
-    
+    var input, newItem;
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
     console.log(input);
     // 2. Add the item to the budget controller
-    
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value); //*----from budget 
     // 3. Add the item to the UI
     
     // 4. Calculate the budget
